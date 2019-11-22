43da6430a0172f25511277d5a7ca09a3a4b32b84
Thambaru Wijesekara <thambaru@gmail.com>
2019-11-22 17:19:56 +0530

    Version 0.0.5

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -7,7 +7,7 @@ chrome.extension.sendMessage({}, function (response) {
 
             var containerEl = $("#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div");
             var standardEl = $("#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div > div > div:nth-child(1) > div > div > div:nth-child(4) > h6");
-			var validTill = $("#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > div > div > div:nth-child(4) > p > em").text();
+            var validTill = $("#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > div > div > div:nth-child(4) > p > em").text();
 
             var regext = /(\d+\.\d+GB)/g;
 
@@ -20,7 +20,7 @@ chrome.extension.sendMessage({}, function (response) {
             var totalUsed = parseFloat(totalText[0]);
             var totalAll = parseFloat(totalText[1]);
 
-			$('#tnwsltntd-container').remove();
+            $('#tnwsltntd-container').remove();
             containerEl.append("<div class='row' id='tnwsltntd-container'>");
 
             if (!(isNaN(totalUsed) || isNaN(totalAll) || isNaN(standardUsed) || isNaN(standardAll))) {
@@ -30,7 +30,7 @@ chrome.extension.sendMessage({}, function (response) {
                 var nightRemaining = (nightAll - nightUsed).toFixed(1);
                 var nightRemainingPercentage = ((nightRemaining / nightAll) * 100).toFixed(0);
 
-                $('#tnwsltntd-container').html("<div class='col-lg-12 col-md-12'><div class='sc-kgAjT kziELt'><div class='inner-node'><h6 class='sc-dnqmqq hZAKPK'>Night Data</h6><div style='margin-top: 10px;'></div><div style='position: relative;'><svg width='180' height='180' viewBox='-25 -25 400 400'><circle stroke='rgba(20,128,225,0.2)' cx='175' cy='175' r='175' stroke-width='40' fill='none'></circle><circle stroke='#2597D8' transform='rotate(-90 175 175)' cx='175' cy='175' r='175' stroke-dasharray='1100' stroke-width='40' stroke-dashoffset='1100' stroke-linecap='butt' fill='none' style='stroke-dashoffset:" + (1100-(100-nightRemainingPercentage/100/1100)) + "; transition: stroke-dashoffset 1s ease-out 0s;'></circle><text fill='#2597D8' x='50%' y='46%' dx='-25' text-anchor='middle' class='remaining-int'>" + nightRemainingPercentage + "<tspan dx='10'>%</tspan></text></svg><p class='remaining-text'>Remaining</p></div><div style='margin-top: -40px;'><h6 id='tnwsltntd-text' class='sc-dnqmqq jZsdgY'>Night Time: Please refresh the page</h6><p style='font-size: 12px; margin: 0px; font-weight: 600; color: rgba(255, 95, 88, 0.9);'><em>"+validTill+"</em></p></div></div></div></div>");
+                $('#tnwsltntd-container').html("<div class='col-lg-12 col-md-12'><div class='sc-kgAjT kziELt'><div class='inner-node'><h6 class='sc-dnqmqq hZAKPK'>Night Data</h6><div style='margin-top: 10px;'></div><div style='position: relative;'><svg width='180' height='180' viewBox='-25 -25 400 400'><circle stroke='rgba(20,128,225,0.2)' cx='175' cy='175' r='175' stroke-width='40' fill='none'></circle><circle stroke='#2597D8' transform='rotate(-90 175 175)' cx='175' cy='175' r='175' stroke-dasharray='1100' stroke-width='40' stroke-dashoffset='1100' stroke-linecap='butt' fill='none' style='stroke-dashoffset:" + (1100 - (nightRemainingPercentage * 11)) + "; transition: stroke-dashoffset 1s ease-out 0s;'></circle><text fill='#2597D8' x='50%' y='46%' dx='-25' text-anchor='middle' class='remaining-int'>" + nightRemainingPercentage + "<tspan dx='10'>%</tspan></text></svg><p class='remaining-text'>Remaining</p></div><div style='margin-top: -40px;'><h6 id='tnwsltntd-text' class='sc-dnqmqq jZsdgY'>Night Time: Please refresh the page</h6><p style='font-size: 12px; margin: 0px; font-weight: 600; color: rgba(255, 95, 88, 0.9);'><em>" + validTill + "</em></p></div></div></div></div>");
                 $("#tnwsltntd-text").text(nightText);
             }
             tnwx++;
